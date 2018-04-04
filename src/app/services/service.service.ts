85fc91498fa6be211f859b5a1465476e3b07c1cc
root <root@localhost.localdomain>
2018-04-04 18:40:57 +0300

    changed http to https

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -14,12 +14,12 @@ export class ServiceService {
   constructor(private http: Http) { }
 
   getCameraType(){
-  	let url = 'http://psms.herokuapp.com/camera_types/'
+  	let url = 'https://psms.herokuapp.com/camera_types/'
   	return this.http.get(url).map(response=>response.json())
   }
 
   newCameraType(name:any){
-  	let url = 'http://psms.herokuapp.com/new_type/'
+  	let url = 'https://psms.herokuapp.com/new_type/'
     let headers = new Headers({
       'Content-Type' : 'application/json',
     });
@@ -27,7 +27,7 @@ export class ServiceService {
   }
 
   listCategory(id) {
-    let url = 'http://psms.herokuapp.com/camera_types/'+id+'/'
+    let url = 'https://psms.herokuapp.com/camera_types/'+id+'/'
     let headers = new Headers({
       'Content-Type' : 'application/json',
     });
@@ -35,7 +35,7 @@ export class ServiceService {
   }
 
   newCamera(name:any){
-    let url = 'http://psms.herokuapp.com/camera_detail_create/'
+    let url = 'https://psms.herokuapp.com/camera_detail_create/'
     let headers = new Headers({
       'Content-Type' : 'application/json',
     });
@@ -45,17 +45,17 @@ export class ServiceService {
   
 
   // getStocks(){
-  //   let url = 'http://127.0.0.1:8000/proc/stock/list/';
+  //   let url = 'https://127.0.0.1:8000/proc/stock/list/';
   //   return this.http.get(url).map(response=> response.json())
   // }
 
   // getStockList(id) {
-  //   let url = 'http://127.0.0.1:8000/proc/stock/get/'+id+'/';
+  //   let url = 'https://127.0.0.1:8000/proc/stock/get/'+id+'/';
   //   return this.http.get(url).map(response=>response.json())
   // }
 
   // newStockProduct(stockproduct:any) {
-  //   let url = 'http://127.0.0.1:8000/proc/product/';
+  //   let url = 'https://127.0.0.1:8000/proc/product/';
   //   let headers = new Headers({
   //     'Content-Type' : 'application/json',
   //   });
